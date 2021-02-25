89b1eadc1a98ff131a2cc13489ad2498211c9f92
Werner <wernercastanaza@gmail.com>
2021-02-25 12:37:37 -0500

    added bootstrap, and started implementing it on the form for the contact page

--- react-website-yt-1/src/components/Form.js
+++ react-website-yt-1/src/components/Form.js
@@ -1,36 +1,43 @@
 import React from 'react'
-import { Button }  from './Button';
-import './Form.css'
+import './Form.css';
+import { Container, Row, Col} from 'react-bootstrap'; 
 
 function Form() {
     return (
-        <div className='container'>
-           <section className='form-heading'>
-                <h1>Mahalo</h1>
-                <p>Ohana means family, family means nobody gets left behind!</p>
-           </section>
-           <section className='form-container'>
-               <form className='form'>
-                    <div className='form-item'>
-                        <div className='item'>
-                            <label>Name:</label>
-                            <input></input>
-                        </div>
-                        <div className='item'>
-                            <label>Email:</label>
-                            <input></input>
-                        </div>
-                    </div>
-                    <div className='form-item'>
-                        <div className='item'>
-                            <label>MSG:</label>
-                            <textarea></textarea>
-                        </div>
-                    </div>
-                    <Button src='contact' buttonStyle='btn--outline'>Send!</Button>
-                </form>
-           </section>            
-        </div>
+        <Container>
+         <section className='form-heading'>
+            <Row>
+            <Col className="text-center">
+                <h1>Say Hi!!</h1>
+            </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>Feel free to reachout to me for any inquaries :)</p>
+                </Col>
+            </Row>
+         </section>        
+         <section>
+            <Container  className='form'>
+                <Row>
+                    <Col class="col-6">
+                        <Row>
+                            <label>Name: </label>
+                            <input type='text'></input>
+                        </Row>
+                        <Row>
+                            <label>Email: </label>
+                            <input type='text'></input>
+                        </Row>
+                    </Col>
+                    <Col class="col-6">
+                        <label>Email: </label>
+                        <input type='text'></input>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+      </Container>
     )
 }
 
---
+++ react-website-yt-1/src/components/temp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function temp() {
+    return (
+        <div>
+            
+            
+        </div>
+    )
+}
+
+export default temp
